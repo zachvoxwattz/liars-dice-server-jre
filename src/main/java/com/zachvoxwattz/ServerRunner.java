6726deae04cb25f409de17a4a58aa501485a60a0
zachvoxwattz <khavo0704@gmail.com>
2024-04-25 21:51:55 +0700

    Initial structure push commit

---
+++ src/main/java/com/zachvoxwattz/ServerRunner.java
@@ -0,0 +1,10 @@
+package com.zachvoxwattz;
+
+/**
+ * Entry point of this Java server.
+ */
+public class ServerRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
