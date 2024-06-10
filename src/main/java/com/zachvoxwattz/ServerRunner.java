b808f3b2bd6525878c4a9a6303ebe49f22f2bcda
zachvoxwattz <khavo0704@gmail.com>
2024-06-11 02:42:32 +0700

    Port check push

--- src/main/java/com/zachvoxwattz/ServerRunner.java
+++ src/main/java/com/zachvoxwattz/ServerRunner.java
@@ -1,8 +1,12 @@
 package com.zachvoxwattz;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.corundumstudio.socketio.Configuration;
+import com.zachvoxwattz.core.GameServer;
 import com.zachvoxwattz.utils.ValueChecker;
 
 /**
@@ -21,19 +25,40 @@ public class ServerRunner {
             // Processes the second passed argument as server's debug mode.
             if (ValueChecker.isBoolean(args[1])) {
                 debugMode = Boolean.parseBoolean(args[1]);
-                if (debugMode) {
-                    mainLogger.trace("Server debug mode enabled.");
-                    mainLogger.debug("Server debug mode enabled.");
-                    mainLogger.info("Server debug mode enabled.");
-                    mainLogger.error("Server debug mode enabled.");
-                    mainLogger.warn("Server debug mode enabled.");
-                    mainLogger.fatal("Server debug mode enabled.");
-                }
+                if (debugMode) mainLogger.debug("Server debug mode enabled.");
             }
+
             // Processes the first passed argument as server's target port.
             if (ValueChecker.isInteger(args[0])) {
                 serverPort = Integer.parseInt(args[0]);
+                if (debugMode) mainLogger.debug("Assigned port number " + serverPort + " to the server.");
+            }
+            else {
+                if (debugMode) mainLogger.debug("Assigned default port number " + serverPort + " to the server.");
             }
         }
+
+        // Constructs a Configuration object to be used later.
+        var serverConfiguration = new Configuration();
+        serverConfiguration.setHostname("0.0.0.0");
+        serverConfiguration.setPort(serverPort);
+
+        // Checking whether the designated port is available.
+        var portIsAvailable = false;
+        if (debugMode) mainLogger.debug("Checking port " + serverPort + " availability...");
+        while (!portIsAvailable) {
+            portIsAvailable = GameServer.portCheck(serverPort);
+            if (debugMode && !portIsAvailable) mainLogger.debug("Port " + serverPort + " is currently occupied... Retrying...");
+
+            try { TimeUnit.SECONDS.sleep(5); }
+            catch (Exception e) {}
+        }
     }
+
+    public static Runnable serverStarter = new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    };
 }
--- src/main/java/com/zachvoxwattz/core/GameServer.java
+++ src/main/java/com/zachvoxwattz/core/GameServer.java
@@ -1,5 +1,7 @@
 package com.zachvoxwattz.core;
 
+import java.net.ServerSocket;
+
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOServer;
 
@@ -10,11 +12,27 @@ public class GameServer extends SocketIOServer {
 
     /**
      * Constructor initializing the Socket.IO server.
-     * @param configuration Object containing many properties
-     * for the server to boot and run.
+     * @param configuration Object containing some crucial
+     * properties for the server to boot and run.
      */
-    public GameServer(Configuration configuration) {
-        super(configuration);
+    public GameServer(Configuration serverConfig) {
+        super(serverConfig);
     }
-    
+
+    /**
+     * Checks the availability of the given port. 
+     * This is a pre-initialization check to make
+     * sure the server can start normally.
+     * @param port The value to be checked.
+     * @return {@code true} if the port is available. Otherwise, {@code false}.
+     */
+    public static boolean portCheck(int port) {
+        try {
+            new ServerSocket(port).close();
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
 }
